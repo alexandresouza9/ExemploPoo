e50246d023230737065ec419f727e17275f90ee8
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-10-28 16:46:12 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 ﻿using ExemploPoo.Models;
 
-
-ContaCorrente c1 = new ContaCorrente(123, 1000);
-
-c1.ExibirSaldo();
-c1.Sacar(5000);
-c1.ExibirSaldo();
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
 
 
 
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-
-
-
-
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "Alexandre";
 // p1.Idade = 40;
